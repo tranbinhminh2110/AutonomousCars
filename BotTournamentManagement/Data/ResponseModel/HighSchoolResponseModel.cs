69dc8411c9653712588269181413c4562be35ce2
trungtin962k <trungtin9620@gmail.com>
2024-02-02 11:26:24 +0700

    return Id for response model

--- BotTournamentManagement/Data/ResponseModel/HighSchoolResponseModel.cs
+++ BotTournamentManagement/Data/ResponseModel/HighSchoolResponseModel.cs
@@ -2,6 +2,7 @@
 {
     public class HighSchoolResponseModel
     {
+        public string Id { get; set; }
         public string KeyId { get; set; }
         public string HighSchoolName { get; set; }
     }
--- BotTournamentManagement/Data/ResponseModel/TeamResponseModel.cs
+++ BotTournamentManagement/Data/ResponseModel/TeamResponseModel.cs
@@ -2,6 +2,7 @@
 {
     public class TeamResponseModel
     {
+        public string Id { get; set; }
         public string KeyId { get; set; }
         public string TeamName { get; set; }
         public string HighSchoolId { get; set; }
