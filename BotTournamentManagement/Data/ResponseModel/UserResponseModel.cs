ce7b9940ac7076bf7be20bc69eecc31ce9d68f7d
Nguyen Trung Tin <trungtin9620@gmail.com>
2024-03-15 11:28:53 +0700

    display password

--- BotTournamentManagement/Data/ResponseModel/UserResponseModel.cs
+++ BotTournamentManagement/Data/ResponseModel/UserResponseModel.cs
@@ -7,6 +7,7 @@
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string FullName { get; set; }
+        public string Password { get; set; }
         public string Role { get; set; }
     }
 }
